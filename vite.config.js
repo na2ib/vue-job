d5c68e4f512681c37bea9673e99a7d6e93ec7c50
Nazib <onlynazib@gmail.com>
2025-11-20 20:19:14 +0600

    tailwind error fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { fileURLToPath, URL } from "node:url";
-
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [vue(), vueDevTools()],
+  //base: "./",
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   server: {
     port: 3000,
     proxy: {

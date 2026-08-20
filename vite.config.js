0a7e9d4b0c73c45c0696274616db63b11cf5c46d
Nazib <onlynazib@gmail.com>
2026-08-20 16:42:32 +0600

    fix action

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vue-job/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {

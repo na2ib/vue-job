3d7155875c7c1475ee079b516c4a1fc113b15a59
Nazib <onlynazib@gmail.com>
2026-08-20 16:14:48 +0600

    github action

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
+// vite.config.js
 import { fileURLToPath, URL } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
-// https://vite.dev/config/
 export default defineConfig({
-  //base: "./",
+  base: '/vue-job/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
